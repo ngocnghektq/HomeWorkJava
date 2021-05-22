2a94cdfa305a97d2c9dea107d60592d70fe6c90e
ngocnghektq <mathiduyen2403@gmail.com>
2021-05-22 15:46:22 +0700

    update bai 7

--- Homework7 update/src/Main.java
+++ Homework7 update/src/Main.java
@@ -26,13 +26,7 @@ public class Main {
 
         }
         //in pan tu mang
-        System.out.println("Mang vua nhap la:");
-        for (int i = 0; i < row; i++) {
-            for (int j = 0; j < col; j++) {
-                System.out.print(arr[i][j] + "\t");
-            }
-            System.out.println();
-        }
+        inPhanTuMang(arr, row, col);
         //In phan tu tren duong cheo chinh
         //tinh tong
         int tong = 0;
@@ -54,24 +48,43 @@ public class Main {
         int tem;
         //int i = 1;
         System.out.println("Mang sau khi sap xep hang 2");
-        for (int i = 1;i<2 ;i++ ) {
+//        for (int i = 1;i<2 ;i++ ) {
 
-            for (int j = 0; j < col - 1; j++) {
-                if (arr[i][j] > arr[i][j + 1]) {
-                    tem = arr[i][j];
-                    arr[i][j] = arr[i][j + 1];
-                    arr[i][j + 1] = tem;
+        for (int j = 0; j < col - 1; j++) {
+            for (int m=j+1; m<col; m++) {
+                if (arr[1][j] > arr[1][m]) {
+                    tem = arr[1][j];
+                    arr[1][j] = arr[1][m];
+                    arr[1][m] = tem;
 
-                   // System.out.print(arr[i][j] + "\t");
+                    // System.out.print(arr[i][j] + "\t");
 
                 }
-
-                 System.out.print(arr[i][j] + "\t");
-
             }
 
+            // System.out.print(arr[i][j] + "\t");
+
+        }
+        System.out.println("Mang vua Sắp xếp là:");
+        for (int k = 0; k < row; k++) {
+            for (int l = 0; l < col; l++) {
+                System.out.print(arr[k][l] + "\t");
+            }
+            System.out.println();
+        }
+
+    }
+
+    private static void inPhanTuMang(int[][] arr, int row, int col) {
+        System.out.println("Mang vua nhap la:");
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                System.out.print(arr[i][j] + "\t");
+            }
+            System.out.println();
         }
     }
+//    }
 }
 
 
