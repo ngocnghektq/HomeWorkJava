ac0f3b925d763b0ea4523cf4b28e8855f9a7c8ac
ngocnghektq <mathiduyen2403@gmail.com>
2021-06-01 22:14:21 +0700

    Bai tap buoi 12

---
+++ Homework12/src/test/java/HomeWorkTest.java
@@ -0,0 +1,50 @@
+import org.assertj.core.api.Assertions;
+import org.junit.Assert;
+import org.junit.Test;
+import static org.assertj.core.api.Assertions.withPrecision;
+import org.junit.jupiter.api.*;
+
+public class HomeWorkTest {
+
+    @Test
+    public void KtEmail()
+    {
+        String so = "asdsff@com.vn";
+        Assertions.assertThat(so).containsOnlyOnce("@")
+               .doesNotContain("..")
+                .doesNotStartWith("@")
+                .doesNotEndWith("@")
+                .containsPattern("^[a-zA-Z][\\w-]+[\\w.]+@([\\w]+\\.[\\w]+|[\\w]+\\.[\\w]{2,}\\.[\\w]{2,})$");
+        //truoc @ van co (.-_ ngan cach cac text truoc @
+    }
+    @Test
+    public void ktCanNangKG()
+    {
+        String donviKG = "48.5kg";
+
+        Assertions.assertThat(donviKG).isNotEmpty()
+                .doesNotStartWith("0") // can nang khong duoi 1kg vi tre so sinh cung tren 1kg :)
+                .containsPattern("^[0-9]+[.]+[0-9]+[a-z&&[kg]]"); //Khong gioi han hasSize vi co nguoi 50kg co nguoi tren 100 kg
+        //Khong can cac dieu kien doesNotContain vi dieu kien contain da cover duoc het case
+    }
+    @Test
+    public void ktCanNangDonVigam(){
+        String gam= "48500g";
+        Assertions.assertThat(gam).isNotEmpty()
+                .doesNotStartWith("0") // can nang khong duoi 1kg vi tre so sinh cung tren 1kg :)
+                .containsPattern("^[0-9]+[0-9]+[a-z&&[g]]");//Khong gioi han hasSize vi co nguoi 50kg co nguoi tren 100 kg
+    }
+    @Test
+    public void ktCMT(){
+        String [] soCMT={"070885432","070885436","070885437", "070885437","070885437"};
+        for (int i=0; i<soCMT.length; i++){
+            Assertions.assertThat(soCMT[i]).isNotEmpty()
+    //                .startsWith(0)  //Nhieu vung CMT k bat dau bang so 0
+                    .hasSize(9)
+                    .containsPattern("[0-9]");
+
+        }
+
+
+    }
+}
